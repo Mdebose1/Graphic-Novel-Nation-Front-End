928222d0fbda33a49851e89d3ae5141d1578ab7f
Mikkia DeBose <kiabo1995@gmail.com>
2022-06-09 15:54:04 -0500

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Navbar } from './components/Navbar'
+
+export const App = () => {
+  return (
+  <>
+    <header>
+      <Navbar />
+    </header>
+
+    <main className="container mt-4">
+     
+
+    </main>
+
+    <footer>
+      
+    </footer>
+  </>
+
+  )
+}
+
+
+ 
---
+++ react/src/views/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div className="row">
+            <div className="col-md-12">
+                <h1>Hello, Lana!</h1>
+
+                <form action="">
+                    <div className="row">
+                        <div className="col-md-10">
+                            <div className="form-group">
+                                <input type="text" className="form-control" name="Post Content" id="" aria-describedby="helpId" placeholder="Post Here" />
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <input name="" id="" className="btn btn-primary btn-block" type="submit" value="Submit Post" />
+                        </div>
+                    </div>
+                </form>
+
+                <ul className="list-group mt-5">
+                    <li className="list-group-item">
+                        <p> Post Content</p>
+                        <div>
+                            <small>&mdash; Lana Moorer
+                                <span className="float-right"> a few seconds ago</span>
+                            </small>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>)
+}
